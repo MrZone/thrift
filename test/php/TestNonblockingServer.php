019abacda8a2096926c8afb266381ccc4643ef8e
volca <volcas@gmailcom>
2010-06-19 23:14:06 +0800

    add php test server

---
+++ test/php/TestNonblockingServer.php
@@ -0,0 +1,104 @@
+<?php
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+class TestHandler {
+
+  function testVoid() {
+    print 'testVoid()';
+  }
+
+  function testString($str) {
+    print sprintf('testString(%s)', $str);
+    return $str;
+  }
+
+  function testByte($byte) {
+    print sprintf('testByte(%d)', $byte);
+    return $byte;
+  }
+
+  function testI16($i16) {
+    print sprintf('testI16(%d)', $i16);
+    return $i16;
+  }
+
+  function testI32($i32) {
+    print sprintf('testI32(%d)', $i32);
+    return $i32;
+  }
+
+  function testI64($i64) {
+    print sprintf('testI64(%d)', $i64);
+    return $i64;
+  }
+
+  function testDouble($dub) {
+    print sprintf('testDouble(%f)', $dub);
+    return $dub;
+  }
+
+  function testStruct($thing) {
+    print sprintf('testStruct({%s, %d, %d, %d})', $thing->string_thing, $thing->byte_thing, $thing->i32_thing, $thing->i64_thing);
+    return $thing;
+  }
+
+  function testException($str) {
+    print sprintf('testException(%s)', $str);
+    if ($str == 'Xception') {
+      $x = Xception();
+      $x->errorCode = 1001;
+      $x->message = $str;
+      throw $x;
+    } elseif ($str == "throw_undeclared") { 
+	throw ValueError("foo");
+}
+  }
+
+  function testOneway($seconds) {
+    print sprintf('testOneway(%d) => sleeping...', $seconds);
+    sleep($seconds);
+    print 'done sleeping';
+  }
+
+  function testNest($thing) {
+    return $thing;
+  }
+
+  function testMap($thing) {
+    return $thing;
+  }
+
+  function testSet($thing) {
+    return $thing;
+  }
+
+  function testList($thing) {
+    return $thing;
+  }
+
+  function testEnum($thing) {
+    return $thing;
+  }
+
+  function testTypefunction($thing) {
+    return $thing;
+  }
+
+}
